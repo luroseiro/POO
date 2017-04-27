4f8a0ba29918b7a2751986b7c57ba6ee641f3f18
Luiz Roseiro <luuroseiro@gmail.com>
2017-04-27 19:35:07 -0300

    ADO #08

--- src/ADO_08/Main.java
+++ src/ADO_08/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Exemplo 1:\n");
+        System.out.println("Exemplo 1: (x = 5 e y = 2)\n");
         NumeroInteiro ex1 = new NumeroInteiro(5, 2);
         ex1.soma();
         ex1.subtrai();
@@ -12,7 +12,7 @@ public class Main {
         ex1.divide();
         System.out.println();
 
-        System.out.println("Exemplo 2:\n");
+        System.out.println("Exemplo 2: (x = 4 e y = 0)\n");
         NumeroInteiro ex2 = new NumeroInteiro(4,0);
         ex2.soma();
         ex2.subtrai();
